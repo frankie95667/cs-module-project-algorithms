a34c6e64287527904468b98ac354d39e37076929
Anthony Kawa <anthonyk2020@gmail.com>
2020-07-23 10:08:01 -0700

    added cache

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,16 +2,21 @@
 Input: an integer
 Returns: an integer
 '''
-from itertools import combinations
-def eating_cookies(n):
-    if n == 0:
+
+def eating_cookies(n, cache={}):
+    if n < 0:
+        return 0
+    elif n == 0:
         return 1
-    elif n > 0 and n < 3:
-        return n
+    elif n in cache:
+        return cache[n]
     else:
-        return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+        cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+        return cache[n]
+    # else:
+    #     return eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 50
-
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    cache = [0 for _ in range(num_cookies + 1)]
+    print(f"There are {eating_cookies(num_cookies, cache)} ways for Cookie Monster to each {num_cookies} cookies")
