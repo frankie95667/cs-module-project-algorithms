0598c111b2abba691793c7e7aa9b7f980f11a272
Anthony Kawa <anthonyk2020@gmail.com>
2020-07-23 19:54:16 -0700

    added queue for highest values at i index and just compared values at every index with the first element of the queue

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,18 +2,50 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-from collections import deque
 def sliding_window_max(nums, k):
-    # assign empty list to maxs
-    maxs = []
-    # for loop through nums with the end being len(nums) - k
-    for i in range(0, len(nums) - (k - 1)):
-        # find max from index + k and append to maxs
-        current_max = max(nums[i:i+k])
-        maxs.append(current_max)
-    
-    # return maxs
-    return maxs
+    # Initialize Qi with empty list
+    Qi = []
+    # initialize max_arr with empty list
+    max_arr = []
+      
+    # Loop through nums array up to index k-1
+    for i in range(k):
+        
+        # While nums[i] is greater than or equal to nums[Qi[-1]]
+        while Qi and nums[i] >= nums[Qi[-1]]:
+            # Remove last element on Qi list
+            Qi.pop()
+
+        # Add new element at rear of queue 
+        Qi.append(i)
+          
+          
+    # Loop through the rest of the nums array
+    # from nums[k] to nums[len(nusm)-1] 
+    for i in range(k, len(nums)):
+          
+        # The element at the front of the 
+        # queue is the largest element of 
+        # previous window, so append it to max_arr 
+        max_arr.append(nums[Qi[0]])
+          
+        # Remove the elements which are  
+        # out of this window 
+        while Qi and Qi[0] <= i-k:
+            # remove from front of Qi 
+            Qi.pop(0)              
+          
+        # While nums[i] is greater than or equal to nums[Qi[-1]]
+        while Qi and nums[i] >= nums[Qi[-1]]:
+            Qi.pop()
+          
+        # Add current element at the rear of Qi 
+        Qi.append(i)
+      
+    # append the maximum element of last window to max_arr
+    max_arr.append(nums[Qi[0]])
+    return max_arr
+
 
 
 if __name__ == '__main__':
